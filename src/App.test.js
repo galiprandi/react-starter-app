041719b58a5ec8bf1cf9f81194218c2db119813e
German Aliprandi <galiprandi@gmail.com>
2021-06-08 19:10:00 -0300

    test: :sparkles: Default test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('renders welcome message', () => {
-  render(<App />);
-  expect(screen.getByTestId('button')).toBeInTheDocument();
+  const container = render(<App />);
+  screen.getByRole('banner')
+
 });
